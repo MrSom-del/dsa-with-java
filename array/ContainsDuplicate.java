7c7eb97ce093a051693d84630591f041e25c9e51
Som Dutt Rai <somlibert357@gmail.com>
2026-01-28 19:34:46 +0530

    Added Contains Duplicate array problem solution

---
+++ array/ContainsDuplicate.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class Main{
+
+    public static boolean containsDuplicate ( int[] nums){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i<nums.length; i++){
+            if(map.containsKey(nums[i])) return true;
+            map.put(nums[i],5);
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+
+        System.out.println(containsDuplicate(nums));
+    }
+}
+
