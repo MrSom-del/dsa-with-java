3a2021d8397fe6050f2ebf244d8bcc4c196f380e
Som Dutt Rai <somlibert357@gmail.com>
2026-02-27 23:54:08 +0530

    Add BuyAndSellStock class with maxProfit method

---
+++ array/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class BuyAndSellStock {
+    public static int maxProfit(int[] prices) {
+        /*int minPrice = prices[0];
+        int maxProfit = 0;
+        for(int i = 0; i<prices.length; i++){
+            minPrice = Math.min(minPrice, prices[i]);
+            int profit = prices[i] - minPrice;
+            maxProfit = Math.max(profit, maxProfit);
+        }
+        return maxProfit;
+    }
+}
