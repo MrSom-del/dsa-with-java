dc4fa006b60ea46e61f79b637cf3c9335509df69
Som Dutt Rai <somlibert357@gmail.com>
2026-02-28 23:51:07 +0530

    Implement method to find longest substring without duplicates

---
+++ Strings/LongSubStringNoDupli.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class LongSubSNoD {
+    public static int lengthOfLongestSubstring(String s) {
+        Set<Character>chars = new HashSet<>();
+        int maxLen = 0;
+        int l = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(!chars.contains(s.charAt(i))){
+                chars.add(s.charAt(i));
+                maxLen = Math.max(maxLen, i - l + 1);
+            }else{
+                while(chars.contains(s.charAt(i))){
+                    chars.remove(s.charAt(l));
+                    l++;
+                }
+                chars.add(s.charAt(i));
+            }
+        }
+        return maxLen;
+    }
+    static void main() {
+        String s = "abcabcbb";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+}
