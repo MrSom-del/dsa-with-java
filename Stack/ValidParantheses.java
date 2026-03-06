0c57a0076af67b7e2f67d2ef48e8367db6673c00
Som Dutt Rai <somlibert357@gmail.com>
2026-03-06 23:22:31 +0530

    Implement ValidParantheses class with isValid method

---
+++ Stack/ValidParantheses.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class ValidParantheses {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char ch : s.toCharArray()){
+            switch(ch){
+                case '(':
+                    stack.push(')');
+                    break;
+                case '{':
+                    stack.push('}');
+                    break;
+                case '[':
+                    stack.push(']');
+                    break;
+                default:
+                    if(stack.isEmpty() || stack.pop() != ch) return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
