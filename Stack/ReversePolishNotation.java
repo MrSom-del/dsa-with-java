f8606e3b825812597593f7c831acf23d09ac3775
Som Dutt Rai <somlibert357@gmail.com>
2026-03-06 23:23:55 +0530

    Implement Reverse Polish Notation using stacks

---
+++ Stack/ReversePolishNotation.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class RPNStack {
+    public static int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for(String ch : tokens){
+            if(ch.equals("+")){
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(a+b);
+            }
+            else if(ch.equals("-")){
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(b-a);
+            }
+            else if(ch.equals("*")){
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(a*b);
+            }
+            else if(ch.equals("/")){
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(b/a);
+            }
+            else {
+                stack.push(Integer.parseInt(ch));
+            }
+        }
+        return stack.pop();
+    }
+}
