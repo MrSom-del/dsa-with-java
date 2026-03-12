0ad0cada673969190e165527819472db816c6d36
Som Dutt Rai <somlibert357@gmail.com>
2026-03-12 23:53:43 +0530

    Implement FindMinRotSort to find minimum in rotated array

---
+++ array/FindMiniInSortedArray.java
@@ -0,0 +1,19 @@
+public class FindMinRotSort {
+    public static int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length-1;
+        while(low < high){
+            int mid = low + (high - low)/2;
+            if(nums[mid]<=nums[high]) high = mid;
+            else{
+                low = mid + 1;
+            }
+        }
+        return nums[low];
+    }
+
+    static void main() {
+        int[] nums = {3,4,5,1,2};
+        System.out.println(findMin(nums));
+    }
+}
