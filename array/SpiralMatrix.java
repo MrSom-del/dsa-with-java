68c0398af514fa725f73fade0ca554e9757d5de4
Som Dutt Rai <somlibert357@gmail.com>
2026-04-02 23:57:28 +0530

    Implement spiral of matrix

---
+++ array/SpiralMatrix.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> lst = new ArrayList<>();
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int left = 0, right = matrix[0].length - 1;
+        int top = 0, bottom = matrix.length - 1;
+
+        while(top <= bottom && left <= right){
+            //top
+            for(int i =  left; i <= right; i++){
+                lst.add(matrix[top][i]);
+            }
+            top++;
+            //right
+
+            for(int i = top; i <= bottom; i++){
+                lst.add(matrix[i][right]);
+            }
+            right--;
+
+            if(top <= bottom){
+                for(int i = right; i >= left; i--){
+                    lst.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            if(left <= right){
+                for(int i = bottom; i >= top; i--){
+                    lst.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return lst;
+    }
+}
